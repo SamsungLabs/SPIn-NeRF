6f850630d4586d399c7c1f2e7fdc2dfda4162353
KAISTChangmin <lcm914@kaist.ac.kr>
2023-10-30 12:42:57 +0000

    add find & right/left feature

--- InteractiveSegmentator.py
+++ InteractiveSegmentator.py
@@ -14,6 +14,8 @@ class InteractiveSegmentator:
         self.fnames = sorted([f for f in os.listdir(self.args.data_root) if f[-3:].lower() in ['png', 'jpg']])
         self.datalen = len(self.fnames)
         self.curr_idx = 0
+        self.find_idx = ''
+        self.find_mod = False
         assert self.datalen > 0
         os.makedirs(os.path.join(self.args.data_root, "label"), exist_ok=True)
         os.makedirs(os.path.join(self.args.data_root, "label255"), exist_ok=True)
@@ -24,8 +26,9 @@ class InteractiveSegmentator:
         self.predictor.set_image(image)
 
         H, W = image.shape[:2]
-        self.current_mask = np.zeros((H, W, 4))
-        self.zero_mask = np.zeros((H, W, 4))
+        self.zero_axim = np.zeros((H, W, 4))
+        self.zero_mask = np.zeros((H, W), dtype=np.uint8)
+        self.curr_mask = self.zero_mask
 
         self.input_point = []
         self.input_label = []
@@ -35,7 +38,7 @@ class InteractiveSegmentator:
         self.fig = plt.figure(figsize=(10,10))
         self.ax = self.fig.add_subplot(111)
         self.axim = self.ax.imshow(image)
-        self.axim_mask = self.ax.imshow(self.zero_mask)
+        self.axim_mask = self.ax.imshow(self.zero_axim)
         self.text = self.fig.text(0, 0.95, f"1/{self.datalen}", size=20)
 
         self.cid0 = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
@@ -56,7 +59,7 @@ class InteractiveSegmentator:
     def save_mask(self):
         save_path = os.path.join(self.args.data_root, "label", self.f)
         save_path255 = os.path.join(self.args.data_root, "label255", self.f)
-        mask = self.mask[...,None].astype(np.uint8).repeat(3, -1)
+        mask = self.curr_mask[...,None].astype(np.uint8).repeat(3, -1)
         cv2.imwrite(save_path, mask)
         cv2.imwrite(save_path255, mask * 255)
     
@@ -76,8 +79,8 @@ class InteractiveSegmentator:
             point_labels=np.array(self.input_label),
             multimask_output=False,
         )
-        self.mask = mask[0]
-        mask_image = self.mask[...,None] * self.color.reshape(1, 1, -1)
+        self.curr_mask = mask[0]
+        mask_image = self.curr_mask[...,None] * self.color.reshape(1, 1, -1)
         self.axim_mask.set_data(mask_image)
 
         self.plots.append(self.ax.scatter(ix, iy, marker='*', color=marker_color, s=375, edgecolor='white', linewidth=1.25))
@@ -85,33 +88,50 @@ class InteractiveSegmentator:
         self.fig.canvas.flush_events()
 
     def onpress(self, event):
-        if event.key in ['enter', 'backspace']:
-            
+        if event.key in ['enter', 'left', 'right']:
             if event.key == 'enter':
-                self.save_mask()
-                self.curr_idx += 1
-                if self.curr_idx >= self.datalen:
-                    self.fig.canvas.mpl_disconnect(self.cid0)
-                    self.fig.canvas.mpl_disconnect(self.cid1)
-                    plt.close(self.fig)
-                    exit(0)
-            elif event.key == 'backspace':
+                if self.find_mod:
+                    if self.find_idx:
+                        self.curr_idx = min(int(self.find_idx) - 1, self.datalen - 1)
+                        self.find_idx = ''
+                    self.find_mod = False
+                else:
+                    self.save_mask()
+                    self.curr_idx += 1
+                    if self.curr_idx >= self.datalen:
+                        self.fig.canvas.mpl_disconnect(self.cid0)
+                        self.fig.canvas.mpl_disconnect(self.cid1)
+                        plt.close(self.fig)
+                        exit(0)
+            elif event.key == 'left':
                 self.curr_idx = max(self.curr_idx - 1, 0)
-            
+            elif event.key == 'right':
+                self.curr_idx = min(self.curr_idx + 1, self.datalen - 1)
             self.f = self.fnames[self.curr_idx]
             image = self.get_image(self.f)
             self.predictor.set_image(image)
 
             self.input_point = []
             self.input_label = []
+            self.curr_mask = self.zero_mask
 
             self.axim.set_data(image)
-            self.axim_mask.set_data(self.zero_mask)
+            self.axim_mask.set_data(self.zero_axim)
             self.text.set_text(f"{self.curr_idx+1}/{self.datalen}")
             for p in self.plots: p.remove()
             self.plots = []
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
+        elif event.key == 'f':
+            self.find_mod = True
+            self.text.set_text(f"Find with index: ")
+        elif self.find_mod and event.key.isdigit():
+            self.find_idx += event.key
+            self.text.set_text(f"Find with index: {self.find_idx}")
+        elif self.find_idx and event.key == 'backspace':
+            self.find_idx = self.find_idx[:-1]
+            self.text.set_text(f"Find with index: {self.find_idx}")
+        
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
 
 
